cdef6b57e0a0377763683ec183bc3cb4b9860542
AdekunleBamzz <bams.kunle@gmail.com>
2025-05-08 16:39:41 +0100

    Fix: Remove deprecated experimental config from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  // Disable server-side features since we're doing static export
-  experimental: {
-    // Remove appDir as it's now the default in Next.js 14
   }
 }
 
